cafd088a77e1417413bf9679458697866215da9f
costajob <costajob@gmail.com>
2016-03-17 11:24:33 +0100

    Mike - moved log nilling on a before hook

--- test/sitemap/factory_test.rb
+++ test/sitemap/factory_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 require 'sitemap/factory'
 
-Sitemap::logger = Logger::new(nil)
-
 describe Sitemap::Factory do
   let(:categories) { [{:site=>"ae", :gender=>"F", :name=>"women's shoes", :host=>"www.gucci.dev", :id=>62}, {:site=>"at", :gender=>"F", :name=>"women's shoes", :host=>"www.gucci.dev", :id=>62}, {:site=>"it", :gender=>"F", :name=>"women's shoes", :host=>"www.gucci.dev", :id=>62}, {:site=>"fr", :gender=>"F", :name=>"women's shoes", :host=>"www.gucci.dev", :id=>62}, {:site=>"de", :gender=>"F", :name=>"women's shoes", :host=>"www.gucci.dev", :id=>62}, {:site=>"bg", :gender=>"F", :name=>"women's shoes", :host=>"www.gucci.dev", :id=>63}].map! { |attrs| Sitemap::Category::new(attrs) } }
   let(:sorts) { [{:site=>"ae", :gender=>"F", :name=>"women's shoes", :sort => "sneakers non US", :host=>"www.gucci.dev", :id=>62}, {:site=>"at", :gender=>"F", :name=>"women's shoes", :sort => "sneakers non US", :host=>"www.gucci.dev", :id=>62}, {:site=>"it", :gender=>"F", :name=>"women's shoes", :sort => "sneakers non US", :host=>"www.gucci.dev", :id=>62}, {:site=>"fr", :gender=>"F", :name=>"women's shoes", :sort => "sneakers non US", :host=>"www.gucci.dev", :id=>62}, {:site=>"de", :gender=>"F", :name=>"women's shoes", :sort => "sneakers non US", :host=>"www.gucci.dev", :id=>62}, {:site=>"bg", :gender=>"F", :name=>"women's shoes", :sort => "pumps non US", :host=>"www.gucci.dev", :id=>63}].map! { |attrs| Sitemap::Sort::new(attrs) } }
@@ -10,7 +8,10 @@ describe Sitemap::Factory do
   let(:stores) { [{:site=>"ae", :host=>"www.gucci.dev", :name=>"1-dam"}, {:site=>"at", :host=>"www.gucci.dev", :name=>"1-dam"}, {:site=>"de", :host=>"www.gucci.dev", :name=>"1-dam"}, {:site=>"fr", :host=>"www.gucci.dev", :name=>"1-dam"}, {:site=>"it", :host=>"www.gucci.dev", :name=>"1-dam"}, {:site=>"ch", :host=>"www.gucci.dev", :name=>"1-boulevard-de-locean-boulevard-de-la-corniche-ain-diab"}, {:site=>"bg", :host=>"www.gucci.dev", :name=>"1-dam"}].map! { |attrs| Sitemap::Store::new(attrs) } }
   let(:repository) { OpenStruct::new(:paths => [Dir::mktmpdir("sitemap1"), Dir::mktmpdir("sitemap2")], :host => "www.gucci.dev", :categories => categories, :sorts => sorts, :styles => styles, :stores => stores, :disconnect => true) }
   let(:factory) { Sitemap::Factory::new(:sites => " ae,at, it, fr ,de, bg", :repository => repository) }
-  before { def factory.fork; yield; end }
+  before do 
+    def factory.fork; yield; end
+    Sitemap::logger = Logger::new(nil)
+  end
 
   it "must define default attributes" do
     factory.instance_variable_get(:@env).must_equal ENV.fetch("RAILS_ENV", "development")
